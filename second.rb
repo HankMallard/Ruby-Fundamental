31476387f80da6dab6da887117cbe7c2d6ef0e05
howardl01@live.ca <howardl01@live.ca>
2016-05-31 14:45:10 -0400

    Second Try

---
+++ second.rb
@@ -0,0 +1,8 @@
+#calculating tips for a 55 dollars meal
+puts 55.0*.15
+#adding integer and string with plus operator
+"55"+ " dollars"
+#interpolation in a sentence
+"The number you are looking for is #{45628*7839}"
+#boolean operation (True)
+(10<20&&30<29)||!(10==11)
